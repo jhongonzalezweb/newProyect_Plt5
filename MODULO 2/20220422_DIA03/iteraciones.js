ed2ea4bc93616c98327baf244b3e6a7e62e30a65
jhongonzalezweb <jhongonzalezxav@gmail.com>
2022-04-22 14:03:41 -0400

    Iteraciones

---
+++ MODULO 2/20220422_DIA03/iteraciones.js
@@ -0,0 +1,24 @@
+/* Iteraciones 
+
+while
+for
+do  
+
+*/
+
+//Ejercicio
+
+
+console.log("------- Bienvenidos a mi cafe -------");
+
+var mesastope = 20;
+
+var clientesRecibidos = 0;
+
+while (clientesRecibidos < mesastope) {
+
+    clientesRecibidos++
+
+    console.log("Tu mesa asignada es la numero: " + clientesRecibidos);
+
+} 
