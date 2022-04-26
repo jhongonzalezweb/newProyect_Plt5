55cb8f8d577f79a1a3527e3a6288e33610455168
jhongonzalezweb <jhongonzalezxav@gmail.com>
2022-04-26 13:35:29 -0400

    clase 05 mod 2

---
+++ MODULO 2/20220426_DIA05/ejercicio.js
@@ -0,0 +1,23 @@
+/*
+ console.log("Hola");
+
+ var stringX = "Lorem Ipsum es simplemente el texto"
+
+ var arregloX = stringX.split(" ")
+
+ console.log(arregloX);
+ console.log("-----");
+
+ arregloX.forEach(element => {
+     console.log(element);
+ });
+
+*/
+
+var arregloDeNumeros = [1,2,3,4,5,6,7,8,9,0]
+
+arregloDeNumeros.forEach(x => {
+
+    console.log(x);
+    
+});
