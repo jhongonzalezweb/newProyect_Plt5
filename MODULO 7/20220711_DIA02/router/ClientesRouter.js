71ecd45c0a1a6fe96ee718ab5db86265bdba93c4
jhongonzalezweb <jhongonzalezxav@gmail.com>
2022-07-14 23:55:41 -0400

    11/08

--- MODULO 7/20220711_DIA02/router/ClientesRouter.js
+++ MODULO 7/20220711_DIA02/router/ClientesRouter.js
@@ -1,13 +1,20 @@
- const express = require('express');
+const { request } = require('express');
+const express = require('express');
 const router = express.Router();
+const bodyParser = require("body-parser");
+
+
+// parse application/x-www-form-urlencoded
+router.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+router.use(bodyParser.json())
 
 const Cliente = require('../models/cliente');
 
 router.get('/', async (request, response) => {
     try {
-        console.log("arrayClientesDBM");
         const arrayClientesDBM = await Cliente.find();
-        console.log(arrayClientesDBM);
 
         response.render("clientes", {
             arrayClientes: arrayClientesDBM
@@ -17,6 +24,38 @@ router.get('/', async (request, response) => {
         console.log(error);
     }
 });
+
+
+
+// Codigo para dirigir la accion a la pagina crear clientes
+router.get('/crear', (request, response) => {
+    response.render('crear');
+})
+
+
+// Codigo para agregar cliente
+router.post('/agregarCliente', async (request, response) => {
+ 
+    console.log("******************************** x ********************************");
+    const body = request.body;
+    console.log("body", body);
+    console.log("******************************** x ********************************");
+    
+    try {
+        const clienteBD = new Cliente(body);
+        await clienteBD.save();
+
+        response.redirect('/clientes')
+ 
+    } catch (error) {
+        console.log(error);
+    }
+
+})
+
+
+
+
 
 
 module.exports = router;
