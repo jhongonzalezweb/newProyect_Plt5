74b6ccb090b483612fcd3ee9a85e70996a9e125b
jhongonzalezweb <jhongonzalezxav@gmail.com>
2022-07-26 19:53:55 -0400

    first commit

--- MODULO 7/SEGURIDAD/index.js
+++ MODULO 7/SEGURIDAD/index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
@@ -5,28 +6,43 @@ require('dotenv').config();
 
 const app = express();
 
+
 // Capturar los datos del body 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 
 //Conexion a la base de datos
+const uri = `mongodb+srv://${process.env.USERA}:${process.env.PASSWORDA}@cluster0.sjaj7.mongodb.net/${process.env.DBNAMEA}?retryWrites=true&w=majority`
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log( err ));
 
 
-//Codigo de los routers
+//Importacion de router
+const authRoutes = require('./routes/auth');
+
+
+// Codigo de los routers
+app.use('/api/user', authRoutes);
+
+
+// Aplicacion de los routes de index.js
 app.get('/', (request, response) => {
 
     response.json({
         estado: true,
-        mensaje: "Esta todo bien"
-
+        mensaje: "Esta todo funcionando 0ook"
     })
 });
 
 
 // Inicio del servidor
-
 const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`El servidor esta andando en el Puerto ${PORT}`);
 })
+
--- MODULO 7/SEGURIDAD/routes/auth.js
+++ MODULO 7/SEGURIDAD/routes/auth.js
@@ -1,1 +1,69 @@
-const router = require('express');
+const User = require('../models/User');
+ 
+const router = require('express').Router();
+const Joi = require('@hapi/joi');
+const bcrypt = require('bcrypt');
+const { request } = require('express');
+ 
+const schemeRegister = Joi.object({
+    name: Joi.string().min(6).max(255).required(),
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+});
+
+// Codigo para logear a un usuario
+router.post('/login', (request, response) => {
+    response.json({
+        mensaje: 'Entre al login'
+    })
+}),
+
+//Metodo para gegistrar un usuario
+router.post('/register', async (request, response) =>
+{
+    // Validación de la data que nos llega por el request
+ 
+    const { error } = schemeRegister.validate(request.body);
+ 
+    console.log("error", error);
+ 
+    if (error) {
+        return response.status(400).json(
+            {
+                error: error.details[0].message
+            }
+        )
+    }
+
+    // Validacion si correo ya existe en la base de datos
+    const isEmailExist = await User.findOne({ email: request.body.email });
+    if (isEmailExist) {
+        return response.status(400).json({ error: 'Email ya registrado' });
+    }
+ 
+    // Encriptación del password
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(request.body.password, salt);
+ 
+    // Creación de nuestro usuario
+    const user = new User(
+        {
+            name: request.body.name,
+            email: request.body.email,
+            password: password
+        });
+   
+    try {
+        const savedUser = await user.save();
+        response.json({
+            error: null,
+            data: savedUser
+        })
+    } catch (error) {
+        response.status(400).json({error});
+    }
+   
+});
+ 
+module.exports = router;
+
